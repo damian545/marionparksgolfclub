277de4e6fa5bdf0f4c0c51d0a07430a65c995bee
Damian Tapp <dam180@hotmail.com>
2021-07-23 20:44:46 +1000

    added more content

--- marion-park-golf-club/src/components/contact.js
+++ marion-park-golf-club/src/components/contact.js
@@ -5,12 +5,9 @@ class Contact extends Component {
     render() {
         return(
 <div style={{width: "100%", margin: "auto"}}>
-    <Grid className="landing-grid" style={{height: "545px"}}>
+    <Grid className="landing-grid" style={{height: "100%"}}>
         <Cell col={3}>
-        <img 
-         src="/images/mpgc-golf-cart.webp"/>
-         <br/>
-         <br/>
+        
          <img src="/images/mpgc-badge-img.jpg"             
             style={{ height:"175px", width:"175px", borderRadius: "50%"}}/>
          <br/>
@@ -18,18 +15,78 @@ class Contact extends Component {
          <Cell className="social-links"><a href="https://google.com/MarionParkGC">
                 <i className="fa fa-facebook-square" aria-hidden="true" />
             </a></Cell>
-            
+         <img 
+         src="/images/mpgc-golf-cart.webp"/>
+         <br/>
+         <br/>   
+          <img 
+         src="/images/mpgc-tree.webp"
+         style={{height:"300", width:"300px"}}/>
+         <br/>
+         <br/>   
+         <br/>
+         <br/>         
+            <img src="/images/mpgc-hole1.webp"
+         style={{height:"300", width:"300px"}}/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <img src="/images/mpgc-golf-cart2.webp"
+         style={{height:"300", width:"300px"}}/>        
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <img src="/images/mpgc-secondhole.webp"
+         style={{height:"500", width:"320px"}}/>  
         </Cell>
         <Cell className="contact-right-col" col={9}>
         <div className="banner-text">
-        <h1 style={{fontSize: "73px", textAlign:"center", color:"lightyellow"}}>CONTACT US.</h1>
-
+        <h1 style={{fontSize: "73px", textAlign:"center", color:"lightgreen"}}>CONTACT US.</h1>
+        
         <hr/>
-
-        
-
-        
-        </div> 
+<div style={{textAlign:"center"}}>
+<p style={{color:"orange", fontSize:"40px"}}>ADDRESS:</p>
+<p>Marion Park Golf Club Inc.</p>
+<p>29 Clubhouse Road</p>
+<p>Seacliff SA 50</p>
+<p>PRO SHOP PHONE: 08 8298 7454</p>
+<hr/>
+<br/>
+<br/>
+<p style={{color:"orange", fontSize:"40px"}}>EMAIL:</p>
+<p>On-Course Bookings & Queries</p>  
+<p>Pro-Shop Email: marion@btmgolf.com.au</p>  
+<br/>
+<br/>
+<p>Club Membership Matters</p>  
+<p>Club Email: marionparkgc@gmail.com</p> 
+<hr/>
+<br/>
+<br/>
+<p style={{color:"orange", fontSize:"40px"}}>POST:</p>
+<p>PO Box 77</p>
+<p>Oaklands Park SA 5046</p>
+  </div>  
+  <hr/>
+  <h1 style={{fontSize: "73px", textAlign:"center", color:"lightgreen"}}>OPENING HOURS.</h1>
+  <hr/>
+  <p style={{color:"orange", fontSize:"40px"}}>Golf Course</p> 
+  <hr/>
+  <p>Monday</p>  
+  <p>Tuesday</p> 
+  <p>Wednesday</p>
+  <p>Thursday</p>
+  <p>Friday</p>
+  <p>Saturday</p>
+  <p>Sunday</p>
+       </div> 
         </Cell>
     </Grid>
     </div>
--- marion-park-golf-club/src/components/greenfees.js
+++ marion-park-golf-club/src/components/greenfees.js
@@ -7,49 +7,103 @@ class Greenfees extends Component {
         return(
 <div style={{width: "100%", margin: "auto"}}>
     <Grid className="landing-grid" style={{height: "100%"}}>
-    <Cell className="aboutme-left-col" col={3}>
-         <img 
-         src="/images/mpgc-tree.webp"
-         style={{height:"300", width:"300px"}}/>
-         <br/>
-         <br/>
+    <Cell className="greenfee-left-col" col={3}>
+         
          <img src="/images/mpgc-badge-img.jpg"             
             style={{ height:"175px", width:"175px", borderRadius: "50%"}}/>
+         <br/>
          <br/>
          <br/>
          <Cell className="social-links"><a href="https://google.com/MarionParkGC">
                 <i className="fa fa-facebook-square" aria-hidden="true" />
             </a></Cell>
-            
+         <img 
+         src="/images/mpgc-tree.webp"
+         style={{height:"300", width:"300px"}}/>
+         <br/>
+         <br/>   
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+            <img src="/images/mpgc-hole1.webp"
+         style={{height:"300", width:"300px"}}/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <img src="/images/mpgc-golf-cart.webp"
+         style={{height:"300", width:"300px"}}/>        
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <img src="/images/mpgc-secondhole.webp"
+         style={{height:"500", width:"320px"}}/>
         </Cell>
         <Cell className="aboutme-right-col" col={9}>
         <div className="banner-text">
-        <h1 style={{fontSize: "53px", textAlign:"center", color:"lightyellow"}}>Green Fees</h1>
-
+        <h1 style={{fontSize: "73px", textAlign:"center", color:"lightgreen"}}>Green Fees <p>payable in addition to Club Memebership Fees</p></h1>
+        
+<p style={{textAlign:"center"}}>The course green fees are managed by the Course Managers Belair Turf Management.</p>
         <hr/>
-
+        <div style={{textAlign:"center"}}>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9 Holes
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;18 Holes</p>
         
-<p>Monday&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;($12 All day except Public Holidays)</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Monday&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;($12 All day except on Public Holidays)</p>
 
-<p className="gap"></p>
+<p>Tuesday-Friday&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$18
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$23</p>
 
-<p className="gap"></p>
+<p>Weekends & Public Holidays&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$21
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$26 </p>
 
-<p className="gap"></p>
+<p>Under 18 & Juniors&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$12&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$17</p>
 
-<p className="gap"></p>
+<p>Cart Hire&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$26&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$35</p>
 
-<p className="gap"></p>
+<p>Cart Hire (BTM Pass Holder)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$20&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$27</p>
 
-<p className="gap"></p>
+<p>Pull Buggy Hire $5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;with Hire Clubs $15</p>
+<hr/>
+</div>
 
-<p className="gap"></p>
+<p style={{textDecoration:"underline"}}>Become a Member & save $$$.</p>
+<p style={{textDecoration:"underline"}}>Annual Passes Available.</p>
+<p style={{textDecoration:"underline"}}>MPGC Members discounts available.</p>
 
-<p className="gap"></p>
-        
+   <hr/>     
+   <h1 style={{fontSize: "73px", textAlign:"center", color:"lightgreen"}}>BECOME A MEMBER <p>The Club Annual Subscription Fees are payable on the 1st of March each year.</p></h1>
+    <hr/>
+    <p style={{textDecoration:"underline"}}>NO JOINING FEE</p>
 
-        
+    <p>New Full Member&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$145(1st year incudes a unique Members Shirt)</p>
+
+    <p>Full Member&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$110</p>
+
+    <p>Junior Member (18 & under)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$55</p>
+<div className="box">
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;PLEASE NOTE:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Above Membership categories DO NOT include Green Fees</p>
+</div>
+<br/>
+<br/>
+<hr/>
+<br/>
+<br/>
         </div> 
         </Cell>
     </Grid>
